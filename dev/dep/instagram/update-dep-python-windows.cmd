33b3bea65e1aef674da99517b6ae131a5d550423
Matthew Ragan <raganmd@gmail.com>
2019-01-17 08:26:59 -0800

    Set-up and clean up for a consistent approach to handling / building a reusable starting point

---
+++ dev/dep/instagram/update-dep-python-windows.cmd
@@ -0,0 +1,7 @@
+:: Update dependencies
+
+:: make sure pip is up to date
+python -m pip install --upgrade pip
+
+:: install requirements
+pip install -r C:/projects/ragan/touchdesigner/_tools/touchdesigner-template-tox-dev/dev/dep/instagram/requirements.txt --target="C:/projects/ragan/touchdesigner/_tools/touchdesigner-template-tox-dev/dev/dep/python"
